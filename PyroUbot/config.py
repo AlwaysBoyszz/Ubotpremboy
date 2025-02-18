10da68f102a345cd0d55a0a0fa781b61c34f8256
AlwaysBoyszz <dilfafah40@gmail.com>
2025-02-18 12:09:31 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -21,4 +21,4 @@ RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Boysstauuu:Boysstauuu@boysstauuu.m4a2p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002163862717"))
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", " -1002163862717"))
