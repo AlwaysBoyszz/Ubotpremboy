e2b73fe5ff82f8b527b7e504b0c6c0a2c318adb6
AlwaysBoyszz <dilfafah40@gmail.com>
2025-02-20 14:45:51 +0700

    Update ssweb.py

--- PyroUbot/modules/ssweb.py
+++ PyroUbot/modules/ssweb.py
@@ -41,7 +41,7 @@ async def screenshot_handler(client, message):
     if not url.startswith(("http://", "https://")):
         url = "https://" + url
 
-    await message.reply_text("<b><i>Ｌｏａｄｉｎｇ．．.</i></b>")
+    await message.reply_text("<b><i>PROSES KINGZ ♛</i></b>")
 
     image_data = get_ssweb_image(url)
     if not image_data:
